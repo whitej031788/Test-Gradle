4f1b34a781b0225021da768d3a7bcda1a1701dea
Jamie White <jamie.white@sonarsource.com>
2025-12-15 15:03:53 +0000

    Update TaskExecutor.java

--- src/main/java/tester/TaskExecutor.java
+++ src/main/java/tester/TaskExecutor.java
@@ -2,49 +2,25 @@ package tester;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+// 1. ADD THIS IMPORT
+import org.postgresql.ds.PGSimpleDataSource;
 
 public class TaskExecutor {
     private final BlockingQueue<ITask> taskQueue = new LinkedBlockingQueue<>();
     private final Thread workerThread;
-    private final PostgresqlDataSource dataSource;
+
+    // 2. CHANGE CLASS NAME HERE
+    private final PGSimpleDataSource dataSource;
 
     public TaskExecutor() {
         workerThread = new Thread(this::run);
 
         // create a fake use for the database
-        dataSource = new PostgresqlDataSource();
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/test");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres"); 
+        // 3. CHANGE CLASS NAME HERE
+        dataSource = new PGSimpleDataSource();
+        dataSource.setURL("jdbc:postgresql://localhost:5432/test"); // Note: setURL uses capital 'URL'
+        dataSource.setUser("postgres"); // Note: setUsername is usually setUser for PGSimpleDataSource
+        dataSource.setPassword("postgres");
     }
-
-    public void execute(final ITask task) {
-        taskQueue.add(task);
-    }
-
-    public void start() {
-        workerThread.start();
-    }
-
-    public void run() {
-        while (true) {
-            try {
-                final ITask task = taskQueue.take();
-                System.out.println("executing " + task);
-                task.doWork();
-            } catch (final InterruptedException e) {
-                System.out.println("InterruptedException");
-                //Thread.currentThread().interrupt();
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        final TaskExecutor taskExecutor = new TaskExecutor();
-        taskExecutor.start();
-        for (int i = 0; i < 50; i++) {
-            taskExecutor.execute(new TestTask(i));
-        }
-        System.out.println("scheduled 50 tasks");
-    }
+    // ... rest of your methods
 }
